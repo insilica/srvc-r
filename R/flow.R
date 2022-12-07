36010827e16116c27831ad7b2c6b3e39911fa167
Thomas Luechtefeld <tom@insilica.co>
2022-12-06 21:59:16 -0500

    basic functionality done

--- R/flow.R
+++ R/flow.R
@@ -1,20 +1,38 @@
 
-cfg <- \(){ jsonlite::fromJSON(Sys.getenv("SR_CONFIG")) }
+globals <- new.env()
 
-mksock    <- purrr::compose(~ socketConnection(.[[1]],.[[2]],blocking=T), ~ strsplit(.,":")[[1]])
-sr_output <- \(){ mksock(Sys.getenv("SR_OUTPUT"),":") }
-sr_input  <- \(){ mksock(Sys.getenv("SR_INPUT"),":") }
+#' @description get the configuration for the current stage
+#' @export
+config <- \(){ jsonlite::fromJSON(Sys.getenv("SR_CONFIG")) }
 
+#' @description initialize an SRVC stage
+#' @export
+initialize <- function(){
+  mksock <- purrr::compose(~ socketConnection(.[[1]],.[[2]],blocking=T), ~ strsplit(.,":")[[1]])
+  globals$sr_output <- mksock(Sys.getenv("SR_OUTPUT"),":") 
+  globals$sr_input <- mksock(Sys.getenv("SR_INPUT"),":") 
+  withr::defer_parent({close(globals$sr_output); close(globals$sr_input)})
+}
 
-flow_next <- function(type){
+#' @description get an event from the SRVC flow
+#' @param predicate predicate function for filtering events, events that match predicate are returned
+#' @export
+next_event <- function(predicate){
   safejs <- purrr::possibly(jsonlite::parse_json,otherwise=NULL)
 
   while(TRUE){
-    line  <- readLines(sr_input,n=1)  
+    line  <- readLines(globals$sr_input,n=1)  
     event <- safejs(line,auto_unbox=T)
-    if(!is.null(event) && event$type==type){
-      writeLines(line, con=sr_output)
+    if(!is.null(event) && predicate(event)){
+      write_event(line)
       return(event) 
     }
   }
+}
+
+#' @description write an event to the SRVC flow
+#' @param event event to write
+#' @export
+write_event <- function(event){
+  writeLines(event, con=globals$sr_output)
 }
